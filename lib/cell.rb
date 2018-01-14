e9b4fa90e085c022e79da203e8d5e648558475e5
Aleksandra Burkiewicz <aleksandra.burkiewicz@gmail.com>
2018-01-14 22:33:13 +0100

    usuniecie nieczytelnych nazw w pliku cell.rb

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,19 +1,19 @@
 # Structure of cell. Contains coordinates x and y.
 class Cell
   
-  attr_accessor :x, :y
+  attr_accessor :wartosc_x, :wartosc_y
   
   # Constructor.
-  # @param x 
-  # @param y
-  def initialize(x, y)
-    @x = x
-    @y = y
+  # @param wartosc_x 
+  # @param wartosc_y
+  def initialize(wartosc_x, wartosc_y)
+    @wartosc_x = wartosc_x
+    @wartosc_y = wartosc_y
   end
   
   # Operator == overloading. Important to delete cell from Hash.
   def ==(cell)
-    if @x == cell.x && @y == cell.y then
+    if @wartosc_x == cell.wartosc_x && @wartosc_y == cell.wartosc_y then
       true;
     else
       false;
@@ -24,8 +24,8 @@ class Cell
   def hash_code
     prime = 31
     result = 1
-    result = prime * result + x
-    result = prime * result + y
+    result = prime * result + wartosc_x
+    result = prime * result + wartosc_y
     return result;
   end
   
